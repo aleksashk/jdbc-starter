1aec6a702f5956b1d75398470fc7aeac5949c55f
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-01-29 16:49:23 +0300

    ConnectionManager

--- src/com/philimonov/jdbc/starter/util/ConnectionManager.java
+++ src/com/philimonov/jdbc/starter/util/ConnectionManager.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/flight_repository";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "password";
+    private static final String URL_KEY = "db.url";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String PASSWORD_KEY = "db.password";
 
     static {
         loadDriver();
@@ -28,7 +28,7 @@ public class ConnectionManager {
 
     public static Connection open(){
         try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            return DriverManager.getConnection(PropertiesUtil.get(URL_KEY), PropertiesUtil.get(USERNAME_KEY), PropertiesUtil.get(PASSWORD_KEY));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
