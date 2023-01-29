eb28894e76737277434a27eedc4e9b179c1b62a6
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-01-29 19:09:51 +0300

    create schema

--- src/com/philimonov/jdbc/starter/JdbcRunner.java
+++ src/com/philimonov/jdbc/starter/JdbcRunner.java
@@ -12,7 +12,7 @@ public class JdbcRunner {
     public static void main(String[] args) throws SQLException{
         Class<Driver> driverClass = Driver.class;
         String firstRequest = """
-                    drop database game""";
+                    create schema game""";
         try (Connection connection = ConnectionManager.open();
              Statement statement = connection.createStatement()) {
             System.out.println(statement.execute(firstRequest));
