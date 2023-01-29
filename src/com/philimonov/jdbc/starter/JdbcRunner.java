8b24c8e18a84d871221d6a2a7afed1402745e060
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-01-29 19:02:34 +0300

    first request

--- src/com/philimonov/jdbc/starter/JdbcRunner.java
+++ src/com/philimonov/jdbc/starter/JdbcRunner.java
@@ -4,15 +4,24 @@ import com.philimonov.jdbc.starter.util.ConnectionManager;
 import org.postgresql.Driver;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException{
         Class<Driver> driverClass = Driver.class;
         try (Connection connection = ConnectionManager.open()) {
-            System.out.println(connection.getTransactionIsolation());
-        } catch (SQLException e) {
-            e.printStackTrace();
+
+            String sql = "select * from flight";
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("flight_no") + " | " + resultSet.getString("departure_date") + " | " + resultSet.getString("departure_airport_code") + " | " + resultSet.getString("arrival_date") + " | " + resultSet.getString("arrival_airport_code") + " | " + resultSet.getInt("aircraft_id") + " | " + resultSet.getString("status"));
+            }
         }
+
     }
 }
