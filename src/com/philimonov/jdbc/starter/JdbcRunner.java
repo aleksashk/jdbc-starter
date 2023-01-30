366203a6a35d39ac13427af82a0feff72b532307
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-01-30 17:52:16 +0300

    preparedStatement with LocalDateTime

--- src/com/philimonov/jdbc/starter/JdbcRunner.java
+++ src/com/philimonov/jdbc/starter/JdbcRunner.java
@@ -6,14 +6,44 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
+        List<Long> flightsBetween = getFlightsBetween(LocalDate.of(2020, 1, 1).atStartOfDay(), LocalDateTime.now());
+        System.out.println(flightsBetween);
+        System.out.println("----------------------------------");
+
         Long flightId = 3L;
         List<Long> result = getTicketsByFlightId(flightId);
         System.out.println(result);
+    }
+
+    private static List<Long> getFlightsBetween(LocalDateTime start, LocalDateTime end) {
+        String sql = """
+                select id from flight where departure_date between ? and ?
+                """;
+        List<Long> result = new ArrayList<>();
+        try (Connection connection = ConnectionManager.open();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            System.out.println(preparedStatement);
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(start));
+            System.out.println(preparedStatement);
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(end));
+            System.out.println(preparedStatement);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                result.add(resultSet.getLong("id"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+
     }
 
     private static List<Long> getTicketsByFlightId(Long flightId) throws SQLException {
