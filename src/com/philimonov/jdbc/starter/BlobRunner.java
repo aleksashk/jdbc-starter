00660ef7746315177af9437410ba41b811f9b1cc
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-01-31 20:55:50 +0300

    Blob, Clob in postgres

--- src/com/philimonov/jdbc/starter/BlobRunner.java
+++ src/com/philimonov/jdbc/starter/BlobRunner.java
@@ -5,8 +5,10 @@ import com.philimonov.jdbc.starter.util.ConnectionManager;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BlobRunner {
@@ -16,6 +18,22 @@ public class BlobRunner {
         //clob / TEXT(postgres)
         //character large object
         saveImage();
+        getImage();
+    }
+
+    private static void getImage() throws SQLException, IOException{
+        String sql = """
+                select image from aircraft where id = ?
+                """;
+        try(Connection connection = ConnectionManager.open();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setInt(1, 1);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()){
+                byte[] image = resultSet.getBytes("image");
+                Files.write(Path.of("resources", "boing777.jpg"), image, StandardOpenOption.CREATE);
+            }
+        }
     }
 
     private static void saveImage() throws SQLException, IOException {
